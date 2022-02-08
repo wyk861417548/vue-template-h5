5452bd6cb1dd870e3dbbfe0361a340a93b8dc6f1
wyk861417548 <861417548@qq.com>
2022-02-08 15:40:47 +0800

    (2-8)基于better-scroll的滚动配置

---
+++ src/views/ceshi.vue
@@ -0,0 +1,58 @@
+<template>
+  <!-- j-full-curbox 当前元素占满全屏  j-flex-col flex垂直布局 -->
+  <div >
+
+    <header>
+      <button @click="handler">点击我</button>
+
+      <slot :user="info">
+        jjj
+      </slot>
+    </header>
+
+  </div>
+
+</template>
+
+<script>
+export default {
+  props:{
+    xxx:{
+      type:String,
+      default:''
+    },
+    title:{
+      type:String,
+      default:''
+    },
+    content:Object
+  },
+  data () {
+    return {
+      info:{
+        name:"你大爷",
+
+        age:"999"
+      }
+    };
+  },
+
+  created(){
+    this.content.name = "xxx";
+  },
+
+  methods: {
+    handler(){
+      this.$emit("myEvent")
+    }
+  }
+}
+</script>
+<style lang='scss' scoped>
+  header{}
+
+  main{}
+
+  footer{}
+
+</style>
--- src/views/index.vue
+++ src/views/index.vue
@@ -1,19 +1,39 @@
 <template>
   <!-- j-full-curbox 当前元素占满全屏  j-flex-col flex垂直布局 -->
   <div>
-    <BScroll ref="scroll" @change="change" >
+    <!-- <BScroll ref="scroll" @change="change" :vdata="dataList">
       <div class="list" style="pointer-events: auto;" v-for='(item,index) in dataList' :key='index'>
         {{item.name}} -- {{item.age}}
       </div>
-    </BScroll>
+    </BScroll> -->
+    <p>我是父級-{{data.title}}--{{data.content}}-----{{data.age}}</p>
+
+    <Ceshi v-bind="data" @myEvent="handler">
+      <template v-slot="{user}">
+        <p>获取子组件数据{{user.name}}</p>
+      </template>
+    </Ceshi>
   </div>
 
 </template>
 
 <script>
+import Ceshi from "./ceshi.vue"
 export default {
+  components:{
+    Ceshi
+  },
   data () {
     return {
+      data:{
+        xxx:"20",
+        title:"大傻逼",
+        content:{
+          name:"你好",
+          age:30
+        }
+      },
+
       dataList:[],
 
       count:1,
@@ -26,29 +46,40 @@ export default {
   },
 
   mounted(){
-    this.getData()
+    // this.getData()
   },
 
   methods: {
+    handler(){
+      console.log("点击我");
+    },
+
     getData(){
       
       setTimeout(()=>{
         this.count++;
         console.log("this.",this.count,this.dataList);
-        // for (let i = 0; i < 20; i++) {
-        //   this.dataList.push({name:this.count+"---i---"+i,age:i})
+        for (let i = 0; i < 20; i++) {
+          this.dataList.push({name:this.count+"---i---"+i,age:i})
 
-        //   console.log("正在填充数据");
-        // }
+          console.log("正在填充数据");
+        }
         // 注意一定要保证  数据渲染完成
+
         this.$nextTick(()=>{
           this.$refs.scroll.finishPullUp();
-          this.$refs.scroll.status = 1;
-          return;
+          
+          if(this.dataList.length < 1){
+            this.$refs.scroll.status = 1;
+            return;
+          }
+
           if(this.count == 4){
             //上拉加载默认状态 0：可加载 1：无数据 2已结束
             this.$refs.scroll.status = 2;
+            return;
           }
+          
         })
         
       },2000)
